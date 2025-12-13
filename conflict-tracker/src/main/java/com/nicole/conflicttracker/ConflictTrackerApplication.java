859a2223809930eb522a80ac80132b474683e3a4
MelanieBau <nivegb@elpuig.xeill.net>
2025-12-13 22:54:22 +0100

    Relaciones basicas OK. Falta completar el CRUD

--- conflict-tracker/src/main/java/com/nicole/conflicttracker/ConflictTrackerApplication.java
+++ conflict-tracker/src/main/java/com/nicole/conflicttracker/ConflictTrackerApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
+//Inicio de la interfaz
 public class ConflictTrackerApplication {
 
 	public static void main(String[] args) {
