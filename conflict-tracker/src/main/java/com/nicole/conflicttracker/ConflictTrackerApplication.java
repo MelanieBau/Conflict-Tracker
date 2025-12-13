568f5276a4359a695f9c81a1136998a80663b6f5
MelanieBau <nivegb@elpuig.xeill.net>
2025-12-13 20:02:05 +0100

    Creacion de packages

---
+++ conflict-tracker/src/main/java/com/nicole/conflicttracker/ConflictTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.nicole.conflicttracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConflictTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConflictTrackerApplication.class, args);
+	}
+
+}
---
+++ conflict-tracker/src/main/java/com/nicole/conflicttracker/dto.java
@@ -0,0 +1,4 @@
+package com.nicole.conflicttracker;
+
+public class dto {
+}
